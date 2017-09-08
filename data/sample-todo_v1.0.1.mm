1f59fe15a6394b7323293fe385f1c87f098a7444
lequinharay <lequinharay lequinharay@gmail.com>
2017-09-08 13:37:42 +0900

    sample data
    for freemind v1.0.1

---
+++ data/sample-todo_v1.0.1.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+    <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+    <node CREATED="1504839055130" ID="ID_993292246" MODIFIED="1504839088333" TEXT="sample-todo">
+        <node CREATED="1504839060161" ID="ID_494957383" MODIFIED="1504839082323" POSITION="right" TEXT="@inbox">
+            <node CREATED="1504839206891" ID="ID_1421699732" MODIFIED="1504839210115" TEXT="&#x672a;&#x6574;&#x7406;&#x30bf;&#x30b9;&#x30af;"/>
+        </node>
+
+        <node CREATED="1504839088266" ID="ID_769480007" MODIFIED="1504839127630" POSITION="right" TEXT="@Todo">
+            <node CREATED="1504839082767" ID="ID_1881748493" MODIFIED="1504839085833" TEXT="@next">
+                <node CREATED="1504839210980" ID="ID_509980120" MODIFIED="1504839213022" TEXT="&#x6b21;&#x306b;&#x3084;&#x308b;"/>
+            </node>
+            <node CREATED="1504839162909" ID="ID_804825117" MODIFIED="1504839164766" TEXT="@waiting">
+                <node CREATED="1504839213946" ID="ID_801672589" MODIFIED="1504839221187" TEXT="lequinharay&#x304c;&#x304b;&#x3089;&#x306e;&#x9023;&#x7d61;&#x5f85;&#x3061;"/>
+            </node>
+            <node CREATED="1504839131048" ID="ID_70732986" MODIFIED="1504839133094" TEXT="@calendar">
+                <node CREATED="1504839222157" ID="ID_425459516" MODIFIED="1504839235738" TEXT="2200-01-01&#x306b;&#x3084;&#x308b;"/>
+            </node>
+            <node CREATED="1504839172810" ID="ID_1227187340" MODIFIED="1504839174332" TEXT="@project">
+                <node CREATED="1504839236808" ID="ID_1342170004" MODIFIED="1504839240498" TEXT="&#x4e16;&#x754c;&#x5f81;&#x670d;"/>
+            </node>
+        </node>
+
+        <node CREATED="1504839112291" ID="ID_1033523651" MODIFIED="1504839124375" POSITION="right" TEXT="@NotTodo">
+            <node CREATED="1504839195890" ID="ID_658483024" MODIFIED="1504839198252" TEXT="@someday">
+                <node CREATED="1504839242227" ID="ID_670558712" MODIFIED="1504839250317" TEXT="&#x72ac;&#x3092;&#x98fc;&#x3046;"/>
+            </node>
+            <node CREATED="1504839199507" ID="ID_817528690" MODIFIED="1504839202828" TEXT="@reference">
+                <node CREATED="1504839251211" ID="ID_1702722540" MODIFIED="1504839262986" TEXT="lequinharay/freemind-parser"/>
+            </node>
+        </node>
+    </node>
+</map>
